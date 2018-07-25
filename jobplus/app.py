ce29dd36fb363309635fbbe2953e877c14209304
tsunemori-akane <po6271933@outlook.com>
2018-07-25 15:11:57 +0800

    flask-migrate

--- jobplus/app.py
+++ jobplus/app.py
@@ -8,6 +8,8 @@ from flask_migrate import Migrate
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
+    db.init_app(app)
+    Migrate(app, db)
     register_blueprints(app)
     register_extensions(app)
     return app
